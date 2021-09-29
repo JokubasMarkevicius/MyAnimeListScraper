3926cb5b776dc5853a31f205cb4fa6c2c9b6d51d
Jokubas <markeviciusjokubas@gmail.com>
2021-09-29 21:39:20 +0100

    unoptimised but it checks whether an anime has a prequel

--- scraper.py
+++ scraper.py
@@ -1,7 +1,8 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-numberOfAnimeToScrape = 100
+numberOfAnimeToScrape = 50
 urlArray = []
 data = []
 
@@ -12,10 +13,15 @@ def getTitlesAndScores(url, data):
   page = requests.get(url)
   soup = BeautifulSoup(page.content, 'html.parser')
   animeList = soup.find_all('tr', class_='ranking-list')
+  # the code below is slowing down the scraper significantly (likely due to the http requests being made within the for loop)
   for anime in animeList:
     title = anime.find('td', class_='title').select('a')[1].text
     score = anime.find('td', class_='score').select('span')[0].text
-    data.append({'title': title, 'score': score})
+    detailsURL = anime.find('td', class_='title').select('a')[1]['href']
+    detailsPage = requests.get(detailsURL)
+    detailsSoup = BeautifulSoup(detailsPage.content, 'html.parser')
+    prequel = detailsSoup.find(text=re.compile("Prequel:")) == "Prequel:"
+    data.append({'title': title, 'score': score, 'prequel': prequel})
 
   print(len(data))
 
@@ -23,8 +29,11 @@ for url in urlArray:
   getTitlesAndScores(url, data)
 
 file = open('anime.csv', 'w')
-file.write('Title, Score \n')
+file.write('Title, Score, Prequel Exists \n')
 for anime in data:
-  file.write(f"{anime['title']}, {anime['score']} \n")
+  file.write(f"{anime['title']}, {anime['score']}, {anime['prequel']} \n")
 
 file.close()
+
+# https://codereview.stackexchange.com/questions/155681/optimizing-the-speed-of-a-web-scraper
+# Going to use the above page to start optimising the scraper cause it's slow as hell the way it is now
